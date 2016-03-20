a8d4c1cedf00d09d218dba725aad4ca4c4780bd0
Jac <jacalata@gmail.com>
2016-03-20 16:26:29 -0700

    remove schedule handling from app server

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 
-var lastDownloadedRecord = "download.json"
-var downloadRequired = false; // err towards less bandwidth usage
-fs = require('fs');
-fs.readFile(lastDownloadedRecord, function(err, data){
-	if (err) {
-		console.log(err);  //don't download
-	}
-		console.log(data);
-		var downloadDetails = JSON.parse(data);
-		console.log(downloadDetails.key)
-});
-
-var Download = require('download');
- 
-new Download({mode: '755'})
-	.get('https://boltproject.gitlab.io/BoltScraper/schedules.json')
-	.dest('data')
-	.run();
 
 var express = require('express');
 var app = express();
